dd83867f4b63be1bead1ce11a52c3c7aa761a90c
giselaz <giselazaimi@gmail.com>
2026-02-03 14:10:05 +0100

    added Job model and factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\JobPortal;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -21,5 +22,6 @@ class DatabaseSeeder extends Seeder
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
+        JobPortal::factory(100)->create();
     }
 }
